01e3286601bb9597bb11f075ba7c04951ca3c17e
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-06-04 10:38:30 -0700

    feat: allow custom HTTP client injection

--- Sources/OpenAI/Private/URLSessionProtocol.swift
+++ Sources/OpenAI/Private/URLSessionProtocol.swift
@@ -10,20 +10,20 @@ import Foundation
 import FoundationNetworking
 #endif
 
-protocol URLSessionProtocol: InvalidatableSession, URLSessionCombine {
+public protocol URLSessionProtocol: InvalidatableSession, URLSessionCombine {
     func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
     func dataTask(with request: URLRequest) -> URLSessionDataTaskProtocol
-    
+
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
 }
 
 extension URLSession: URLSessionProtocol {
-    func dataTask(with request: URLRequest) -> URLSessionDataTaskProtocol {
+    public func dataTask(with request: URLRequest) -> URLSessionDataTaskProtocol {
         dataTask(with: request) as URLSessionDataTask
     }
-    
-    func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+
+    public func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
         dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
     }
 }
